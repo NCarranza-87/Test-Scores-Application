9833af8dc1bd987470ba86f2822f8da3e5c1dd45
NCarranza-87 <36281270+NCarranza-87@users.noreply.github.com>
2018-07-02 15:50:06 -0700

    Add project files.

---
+++ TestScoreApp/frmTestScore.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace TestScoreApp
+{
+    partial class frmTestScore
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblScoreTotal = new System.Windows.Forms.Label();
+            this.lblScoreCount = new System.Windows.Forms.Label();
+            this.lblAvg = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.txtScoreTotal = new System.Windows.Forms.TextBox();
+            this.txtScoreCount = new System.Windows.Forms.TextBox();
+            this.txtAverage = new System.Windows.Forms.TextBox();
+            this.btnDisplayScores = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnClearScores = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(48, 25);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(38, 13);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "Score:";
+            // 
+            // lblScoreTotal
+            // 
+            this.lblScoreTotal.AutoSize = true;
+            this.lblScoreTotal.Location = new System.Drawing.Point(24, 53);
+            this.lblScoreTotal.Name = "lblScoreTotal";
+            this.lblScoreTotal.Size = new System.Drawing.Size(65, 13);
+            this.lblScoreTotal.TabIndex = 1;
+            this.lblScoreTotal.Text = "Score Total:";
+            // 
+            // lblScoreCount
+            // 
+            this.lblScoreCount.AutoSize = true;
+            this.lblScoreCount.Location = new System.Drawing.Point(17, 83);
+            this.lblScoreCount.Name = "lblScoreCount";
+            this.lblScoreCount.Size = new System.Drawing.Size(69, 13);
+            this.lblScoreCount.TabIndex = 2;
+            this.lblScoreCount.Text = "Score Count:";
+            // 
+            // lblAvg
+            // 
+            this.lblAvg.AutoSize = true;
+            this.lblAvg.Location = new System.Drawing.Point(36, 112);
+            this.lblAvg.Name = "lblAvg";
+            this.lblAvg.Size = new System.Drawing.Size(50, 13);
+            this.lblAvg.TabIndex = 3;
+            this.lblAvg.Text = "Average:";
+            // 
+            // txtScore
+            // 
+            this.txtScore.Location = new System.Drawing.Point(92, 18);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(100, 20);
+            this.txtScore.TabIndex = 4;
+            // 
+            // txtScoreTotal
+            // 
+            this.txtScoreTotal.Location = new System.Drawing.Point(92, 45);
+            this.txtScoreTotal.Name = "txtScoreTotal";
+            this.txtScoreTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtScoreTotal.TabIndex = 5;
+            // 
+            // txtScoreCount
+            // 
+            this.txtScoreCount.Location = new System.Drawing.Point(92, 75);
+            this.txtScoreCount.Name = "txtScoreCount";
+            this.txtScoreCount.Size = new System.Drawing.Size(100, 20);
+            this.txtScoreCount.TabIndex = 6;
+            // 
+            // txtAverage
+            // 
+            this.txtAverage.Location = new System.Drawing.Point(92, 104);
+            this.txtAverage.Name = "txtAverage";
+            this.txtAverage.Size = new System.Drawing.Size(100, 20);
+            this.txtAverage.TabIndex = 7;
+            // 
+            // btnDisplayScores
+            // 
+            this.btnDisplayScores.Location = new System.Drawing.Point(20, 142);
+            this.btnDisplayScores.Name = "btnDisplayScores";
+            this.btnDisplayScores.Size = new System.Drawing.Size(118, 23);
+            this.btnDisplayScores.TabIndex = 8;
+            this.btnDisplayScores.Text = "Display Scores";
+            this.btnDisplayScores.UseVisualStyleBackColor = true;
+            this.btnDisplayScores.Click += new System.EventHandler(this.BtnDisplayScores_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(198, 16);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // btnClearScores
+            // 
+            this.btnClearScores.Location = new System.Drawing.Point(144, 142);
+            this.btnClearScores.Name = "btnClearScores";
+            this.btnClearScores.Size = new System.Drawing.Size(129, 23);
+            this.btnClearScores.TabIndex = 10;
+            this.btnClearScores.Text = "Clear Scores";
+            this.btnClearScores.UseVisualStyleBackColor = true;
+            this.btnClearScores.Click += new System.EventHandler(this.BtnClearScores_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(197, 172);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // frmTestScore
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 204);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClearScores);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnDisplayScores);
+            this.Controls.Add(this.txtAverage);
+            this.Controls.Add(this.txtScoreCount);
+            this.Controls.Add(this.txtScoreTotal);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.lblAvg);
+            this.Controls.Add(this.lblScoreCount);
+            this.Controls.Add(this.lblScoreTotal);
+            this.Controls.Add(this.lblScore);
+            this.Name = "frmTestScore";
+            this.Text = "Score Calculator";
+            this.Load += new System.EventHandler(this.FrmTestScore_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblScoreTotal;
+        private System.Windows.Forms.Label lblScoreCount;
+        private System.Windows.Forms.Label lblAvg;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.TextBox txtScoreTotal;
+        private System.Windows.Forms.TextBox txtScoreCount;
+        private System.Windows.Forms.TextBox txtAverage;
+        private System.Windows.Forms.Button btnDisplayScores;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnClearScores;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ TestScoreApp/frmTestScore.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestScoreApp
+{
+    public partial class frmTestScore : Form
+    {
+        /********************************************************************
+         * For this project, create a from that accpets score from the user, 
+         * displays the total, count and average of the scores, and display 
+         * a dialog box that lists the scores.
+         *******************************************************************/
+
+        //ArrayList to store test scores
+        List<int> testScores = new List<int>();
+
+        public frmTestScore()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// When application is loaded; txtScoreTotal, txtScoreCount
+        /// and txtAverage are invoking the ReadOnly method and if set
+        /// to true. It will only display the corresponding values.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FrmTestScore_Load(object sender, EventArgs e)
+        {
+            txtScoreTotal.ReadOnly = true;
+            txtScoreCount.ReadOnly = true;
+            txtAverage.ReadOnly = true;
+        }
+
+        /// <summary>
+        /// when user clicks on the Add button, a text
+        /// score is add to the ArrayList.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            bool isEmpty = true;
+
+            if (IsPresent(txtScore))
+            {
+                MessageBox.Show("Your score textbox is empty.", "Empty!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                isEmpty = true;
+            }
+            else
+            {
+                isEmpty = false;
+                txtScore.Focus();
+            }
+
+            if (IsDataValid())
+            {
+                //code to check if input data is valid.
+                testScores.Add(Convert.ToInt32(txtScore.Text));
+                MessageBox.Show("Test score have been added.");
+
+                displayScoreTotal();
+                displayScoreCount();
+                displayAverage();
+            }
+
+            if(string.IsNullOrWhiteSpace(txtScore.Text))
+            {
+                MessageBox.Show("Please enter a test score.", "Enter test score!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                txtScore.Clear();
+                txtScore.Focus();
+            }
+
+            txtScore.Clear();
+            txtScore.Focus();
+        }
+
+        /// <summary>
+        /// check to see if the use has enter any
+        /// data in the test score textbox
+        /// </summary>
+        /// <param name="box"></param>
+        /// <returns></returns>
+        private bool IsPresent(TextBox box)
+        {
+            if(txtScore.Text == "")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// check if the user data is a valid input
+        /// </summary>
+        /// <returns> return true if scoreInput is  withing range, return false if 
+        /// scoreInput is not within range</returns>
+        private bool IsDataValid()
+        {
+            //get the user input
+            string scoreInput = txtScore.Text;
+
+            bool IsInputValid = false;
+
+            //check is input valid
+            if (isDecimal(scoreInput))
+            {
+                if(IsWithinRange(Convert.ToDecimal(scoreInput), 0, 100))
+                {
+                    IsInputValid = true;
+                }
+                else
+                {
+                    DialogResult error = MessageBox.Show("Invalid input!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    if(error == DialogResult.OK)
+                    {
+                        txtScore.Clear();
+                        txtScore.Focus();
+                    }
+                }
+            }
+            return IsInputValid;
+        }
+
+        private bool IsWithinRange(decimal testNum, decimal minVal, decimal maxVal)
+        {
+            if (testNum >= minVal && testNum <= maxVal)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool isDecimal(string inputVal)
+        {
+            if(decimal.TryParse(inputVal, out decimal number))
+            {
+                return true;
+            }
+            return false;
+        }
+        /// <summary>
+        /// When Display Scores button is clicked on a
+        /// message box will displays all of the test scores
+        /// in the ArrayList
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnDisplayScores_Click(object sender, EventArgs e)
+        {
+            if (testScores.Count == 0)
+            {
+                //display an message box that shows
+                //an error that states ther are no
+                //elements within the ArrayList
+                MessageBox.Show("There are currently no test score!", "Empty!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                string displayTestScoreList = string.Join("\n", testScores);
+                MessageBox.Show(displayTestScoreList, "Test Scores:", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+
+            
+        }
+
+        /// <summary>
+        /// computes and displays the testScore 
+        /// list as the user enters a test score
+        /// </summary>
+        private void displayAverage()
+        {
+            int sum = 0;
+            int average = 0;
+
+            for (int i = 0; i < testScores.Count; i++)
+            {
+                sum += testScores[i];
+                average = sum / testScores.Count;
+            }
+
+            txtAverage.Text = average.ToString();
+        }
+
+        /// <summary>
+        /// displays the number of elements 
+        /// within the list
+        /// </summary>
+        private void displayScoreCount()
+        {
+            int scoreCount = 0;
+
+            for (int i = 0; i < testScores.Count; i++)
+            {
+                scoreCount = testScores.Count;
+            }
+
+            txtScoreCount.Text = scoreCount.ToString();
+        }
+
+        /// <summary>
+        /// display and computes the total of
+        /// all the scores within the list
+        /// </summary>
+        private void displayScoreTotal()
+        {
+            int scoreTotal = 0;
+
+            for (int i = 0; i < testScores.Count(); i++)
+            {
+                scoreTotal += testScores[i];
+            }
+
+            //print the score total
+            txtScoreTotal.Text = scoreTotal.ToString();
+        }
+
+        /// <summary>
+        /// When Clear Scores button is clicked on the text scores
+        /// in the ArrayList is clear, then a message box will prompt
+        /// the user stating that score are cleared from ArrayList
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnClearScores_Click(object sender, EventArgs e)
+        {
+            testScores.Clear();
+            txtScoreTotal.Clear();
+            txtScoreCount.Clear();
+            txtAverage.Clear();
+            MessageBox.Show("Test scores have been cleared from list!");
+        }
+
+        /// <summary>
+        /// When Exit button is clicked on; BtnExit_Click
+        /// will calls CloseFormApplication()
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            CloseFormApplication();
+        }
+
+        /// <summary>
+        /// When BtnExit_Click calls CloseFOrmApplication()
+        /// the user will be prompt to close the application.
+        /// If res equals Yes, the application will close.
+        /// </summary>
+        private void CloseFormApplication()
+        {
+            DialogResult res = MessageBox.Show("Do you wish to quit?", "Close Appilcation?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if(res == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+}
